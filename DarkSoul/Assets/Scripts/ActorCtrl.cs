d922f63692c4e64dcd8a0999a74c89fdf3355208
CaiZeShen <673544319@qq.com>
2019-03-25 17:37:36 +0800

    增加跑

--- DarkSoul/Assets/Scripts/ActorCtrl.cs
+++ DarkSoul/Assets/Scripts/ActorCtrl.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActorCtrl : MonoBehaviour {
+    public float moveSpeed = 1;
+    public float runMultiplier = 2;
     [SerializeField] private GameObject m_Modle;
     private Animator m_Animator;
     private PlayerInput m_PInput;
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_MovingVec;
 
     private void Awake() {
         m_Animator = m_Modle.GetComponent<Animator>();
         m_PInput = GetComponent<PlayerInput>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     void Start() {
@@ -17,9 +22,18 @@ public class ActorCtrl : MonoBehaviour {
     }
 
     void Update() {
-        m_Animator.SetFloat("forward", m_PInput.dMag);
-        m_Modle.transform.forward = m_PInput.dVec;
+        m_Animator.SetFloat("forward", m_PInput.dMag * (m_PInput.run ? 2 : 1));
+
+        // 设置旋转
+        if (m_PInput.dMag > 0.1f) 
+            m_Modle.transform.forward = m_PInput.dVec ;
+
+        m_MovingVec = m_Modle.transform.forward * m_PInput.dMag * moveSpeed * (m_PInput.run ? runMultiplier : 1);
     }
 
+    private void FixedUpdate() {
+        //m_Rigidbody.position += movingVec * Time.fixedDeltaTime;
+        m_Rigidbody.velocity = new Vector3(m_MovingVec.x, m_Rigidbody.velocity.y, m_MovingVec.z) ;
+    }
 
 }
--- DarkSoul/Assets/Scripts/PlayerInput.cs
+++ DarkSoul/Assets/Scripts/PlayerInput.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
-    public bool inputEnble;
-
+    [Header("===== 按键设置 =====")]
     public string keyUp = "w";
     public string keyDown = "s";
     public string keyLeft = "a";
     public string keyRight = "d";
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
 
+    [Header("===== 信号输出 =====")]
     public float dUp;
     public float dRight;
     public float dMag;
     public Vector3 dVec;
+    public bool run;
+
+    [Header("===== 其他 =====")]
+    public bool inputEnble;
 
     private float m_TargetDUp;
     private float m_VelocityDUp;
@@ -37,5 +45,7 @@ public class PlayerInput : MonoBehaviour {
 
         dMag = Mathf.Sqrt(dUp * dUp + dRight * dRight);
         dVec = dUp * transform.forward + dRight * transform.right;
+
+        run = Input.GetKey(keyA);
     }
 }
